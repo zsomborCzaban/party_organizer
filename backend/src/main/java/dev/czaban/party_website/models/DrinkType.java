f8982ff5ca0ae070409cdc5a885aca31ff6d1364
zsomborDev <zsombor130@gmail.com>
2024-05-27 14:27:07 +0300

    deleted entity, bc we use nosql database

--- backend/src/main/java/dev/czaban/party_website/models/DrinkType.java
+++ backend/src/main/java/dev/czaban/party_website/models/DrinkType.java
@@ -1,18 +1,34 @@
 package dev.czaban.party_website.models;
 
 
-import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "drink_types")
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
 public class DrinkType {
 
-    private int id;
+    @Id
+    private ObjectId id;
     private String drinkType;
 
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getDrinkType() {
+        return drinkType;
+    }
+
+    public void setDrinkType(String drinkType) {
+        this.drinkType = drinkType;
+    }
 }
