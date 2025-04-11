531527fd9759fea3df96e7e790d9bb5dfc804bd3
zsombor <bu6t68@inf.elte.hu>
2025-04-11 15:27:12 +0200

    Remove old presentation tier folder

--- presentation_tier/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { AppRouter } from '../../frontend/src/AppRouter';
-import { NavigationBar } from './components/navigation-bar/NavigationBar';
-
-export const App = () => (
-  <>
-    <NavigationBar />
-    <AppRouter />;
-  </>
-);
