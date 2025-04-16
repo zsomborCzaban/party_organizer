34d82f12488ee7b70d70231e5af4bd8f44912d41
bu6t68 <bu6t68@inf.elte.hu>
2025-04-16 12:34:14 +0200

    scss and partyNavbar mofidied

--- frontend/src/components/navigation-bar/PartyNavigationBar.tsx
+++ frontend/src/components/navigation-bar/PartyNavigationBar.tsx
@@ -1,27 +1,30 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import classes from './NavigationBarDarkTheme.module.scss';
 import { NavigationButton } from './navigation-button/NavigationButton';
+import { useAppSelector } from '../../store/store-helper';
+import { isUserLoggedIn } from '../../store/sclices/UserSlice';
 
 export const PartyNavigationBar = () => {
     const navigate = useNavigate();
     const location = useLocation();
+    const userLoggedIn = useAppSelector(isUserLoggedIn);
     const partyName = localStorage.getItem('partyName') || 'Unexpected error';
 
     return (
         <nav className={classes.navBar}>
             <div className={classes.leftSection}>
-        <span
-            className={classes.pageTitle}
-            onClick={() => navigate('/')}
-        >
-          Party organizer
-        </span>
+                <span
+                    className={classes.pageTitle}
+                    onClick={() => navigate('/')}
+                >
+                    Party organizer
+                </span>
                 <span
                     className={classes.pageTitle}
                     onClick={() => navigate('/partyHome')}
                 >
-          {partyName}
-        </span>
+                    {partyName}
+                </span>
             </div>
 
             <div className={classes.centerSection}>
@@ -30,30 +33,42 @@ export const PartyNavigationBar = () => {
                     navigateToLink='/partyHome'
                     isActive={location.pathname === '/partyHome'}
                 />
-                <NavigationButton
-                    buttonText='Contributions'
-                    navigateToLink='/contributions'
-                    isActive={location.pathname === '/contributions'}
-                />
-                <NavigationButton
-                    buttonText='Hall Of Fame'
-                    navigateToLink='/hallOfFame'
-                    isActive={location.pathname === '/hallOfFame'}
-                />
-                <NavigationButton
-                    buttonText='Cocktails'
-                    navigateToLink='/cocktails'
-                    isActive={location.pathname === '/cocktails'}
-                />
+                {userLoggedIn && (
+                    <>
+                        <NavigationButton
+                            buttonText='Contributions'
+                            navigateToLink='/contributions'
+                            isActive={location.pathname === '/contributions'}
+                        />
+                        <NavigationButton
+                            buttonText='Hall Of Fame'
+                            navigateToLink='/hallOfFame'
+                            isActive={location.pathname === '/hallOfFame'}
+                        />
+                        <NavigationButton
+                            buttonText='Cocktails'
+                            navigateToLink='/cocktails'
+                            isActive={location.pathname === '/cocktails'}
+                        />
+                    </>
+                )}
             </div>
 
             <div className={classes.rightSection}>
-
-                <NavigationButton
-                    buttonText='Profile'
-                    navigateToLink='/profile'
-                    isActive={location.pathname === '/profile'}
-                />
+                {userLoggedIn ? (
+                    <NavigationButton
+                        buttonText='Profile'
+                        navigateToLink='/profile'
+                        isActive={location.pathname === '/profile'}
+                    />
+                ) : (
+                    <button
+                        className={classes.authButton}
+                        onClick={() => navigate('/login')}
+                    >
+                        Sign In
+                    </button>
+                )}
             </div>
         </nav>
     );
