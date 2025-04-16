697fbf4234c2ce20f05d367cb321e1a8f5df654a
bu6t68 <bu6t68@inf.elte.hu>
2025-04-16 13:38:45 +0200

    requirement api class created

--- frontend/src/api/apis/RequirementApi.ts
+++ frontend/src/api/apis/RequirementApi.ts
@@ -1,10 +1,100 @@
 import { DELETE, get, post } from '../../api/Api';
 
 import { getApiUrl } from '../../api/ApiHelper';
-import { Requirement } from '../../data/types/Requirement';
+import {Requirement, RequirementPopulated} from '../../data/types/Requirement';
+import axios, {AxiosInstance, AxiosResponse} from "axios";
 
 const DRINK_REQUIREMENT_PATH = `${getApiUrl()}/drinkRequirement`;
 const FOOD_REQUIREMENT_PATH = `${getApiUrl()}/foodRequirement`;
+
+const handleApiResponse = <T>(response: AxiosResponse<T>): T => {
+    return response.data;
+};
+
+const handleApiError = (error: unknown) => {
+    // TODO: handle errors as needed
+    if (axios.isAxiosError(error)) {
+        console.error(`Axios error: ${error.message}`);
+    } else {
+        console.error(`Unexpected error: ${error}`);
+    }
+};
+
+export type RequirementsResponse = {
+    data: RequirementPopulated[]
+}
+
+export type RequirementResponse = {
+    data: RequirementPopulated
+}
+
+export class RequirementApi {
+    private axiosInstance: AxiosInstance;
+
+    constructor(axiosInstance: AxiosInstance) {
+        this.axiosInstance = axiosInstance;
+    }
+
+    async getDrinkRequirementsByPartyId(partyId: number): Promise<RequirementsResponse | 'error'> {
+        try {
+            const response = await this.axiosInstance.get<RequirementsResponse>(`${DRINK_REQUIREMENT_PATH}/getByPartyId/${partyId.toString()}`)
+            return handleApiResponse(response)
+        } catch (error) {
+            handleApiError(error)
+            return 'error'
+        }
+    }
+
+    async createDrinkRequirement(requestBody: Requirement): Promise<RequirementResponse | 'error'> {
+        try {
+            const response = await this.axiosInstance.post<RequirementResponse>(`${DRINK_REQUIREMENT_PATH}`, requestBody)
+            return handleApiResponse(response)
+        } catch (error) {
+            handleApiError(error)
+            return 'error'
+        }
+    }
+
+    async deleteDrinkRequirement(requirement: number): Promise<RequirementResponse | 'error'> {
+        try {
+            const response = await this.axiosInstance.delete<RequirementResponse>(`${DRINK_REQUIREMENT_PATH}/${requirement.toString()}`)
+            return handleApiResponse(response)
+        } catch (error) {
+            handleApiError(error)
+            return 'error'
+        }
+    }
+
+    async getFoodRequirementsByPartyId(partyId: number): Promise<RequirementsResponse | 'error'> {
+        try {
+            const response = await this.axiosInstance.get<RequirementsResponse>(`${FOOD_REQUIREMENT_PATH}/getByPartyId/${partyId.toString()}`)
+            return handleApiResponse(response)
+        } catch (error) {
+            handleApiError(error)
+            return 'error'
+        }
+    }
+
+    async createFoodRequirement(requestBody: Requirement): Promise<RequirementResponse | 'error'> {
+        try {
+            const response = await this.axiosInstance.post<RequirementResponse>(`${FOOD_REQUIREMENT_PATH}`, requestBody)
+            return handleApiResponse(response)
+        } catch (error) {
+            handleApiError(error)
+            return 'error'
+        }
+    }
+
+    async deleteFoodRequirement(requirement: number): Promise<RequirementResponse | 'error'> {
+        try {
+            const response = await this.axiosInstance.delete<RequirementResponse>(`${FOOD_REQUIREMENT_PATH}/${requirement.toString()}`)
+            return handleApiResponse(response)
+        } catch (error) {
+            handleApiError(error)
+            return 'error'
+        }
+    }
+}
 
 export const getDrinkRequirements = async (partyId: number): Promise<Requirement[]> =>
   new Promise<Requirement[]>((resolve, reject) => {
--- frontend/src/data/types/Requirement.ts
+++ frontend/src/data/types/Requirement.ts
@@ -6,3 +6,12 @@ export interface Requirement {
     quantity_mark: string;
     description?: string;
 }
+
+export interface RequirementPopulated {
+    ID: number;
+    party_id: number
+    type: string;
+    target_quantity: number;
+    quantity_mark: string;
+    description: string;
+}
